12c2b7f69ca6228fda7c41a0b9088d85dcc0331a
Makena <soniamakena02@gmail.com>
2020-09-30 11:39:52 +0300

    uncommented out tests

--- src/test/java/dao/Sql2oUsersDaoTest.java
+++ src/test/java/dao/Sql2oUsersDaoTest.java
@@ -1,107 +1,107 @@
-//package dao;
-//
-//import models.Departments;
-//import models.Users;
-//import org.junit.*;
-//import org.sql2o.Connection;
-//import org.sql2o.Sql2o;
-//
-//
-//import static org.junit.Assert.*;
-//
-//public class Sql2oUsersDaoTest {
-//    private static Sql2oUsersDao usersDao;
-//    private static Sql2oDepartmentsDao departmentsDao;
-//    private static Connection connection;
-//
-//    @BeforeClass
-//    public static void setUp() throws Exception {
-//        String connectionString = "jdbc:postgresql://localhost:5432/organisational_api_test";
-//        Sql2o sql2o = new Sql2o(connectionString, "makena", "barbie");
-//        usersDao = new Sql2oUsersDao(sql2o);
-//        departmentsDao = new Sql2oDepartmentsDao(sql2o);
-//        connection = sql2o.open();
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception {
-//        System.out.println("CLEARING DATABASE!!!!!!");
-//        usersDao.clearAll();
-//        departmentsDao.clearAll();
-//    }
-//
-//    @AfterClass
-//    public static void shutDown(){
-//        System.out.println("SHUTTING DOWN DATABASE!");
-//        connection.close();
-//    }
-//
-//    @Test
-//    public void save_savesUserInformationInTheDatabase() throws Exception {
-//        Users users = setUpUser();
-//        usersDao.save(users);
-//        assertEquals(1, usersDao.findAll().size());
-//    }
-//
-//    @Test
-//    public void findAll_ReturnsAllUsers() {
-//        Users users = setUpUser();
-//        assertEquals(0, usersDao.findAll().size());
-//        usersDao.save(users);
-//        assertEquals(1, usersDao.findAll().size());
-//    }
-//    @Test
-//    public void findById_returnsExistingUsersBasedOnId() {
-//        Users users = setUpUser();
-//        usersDao.save(users);
-//        Users users1 = usersDao.findById(users.getId());
-//        assertEquals(users, users1);
-//    }
-//    @Test
-//    public void delete_deletesUserById(){
-//        Users users = setUpUser();
-//        usersDao.save(users);
-//        assertEquals(1, usersDao.findAll().size());
-//        int idOfFoundUser = usersDao.findById(users.getId()).getId();
-//        usersDao.delete(idOfFoundUser);
-//        assertEquals(0, usersDao.findAll().size());
-//    }
-//    @Test
-//    public void clearAll_DeletesEverything(){
-//        Users users = setUpUser();
-//        usersDao.save(users);
-//        Users users1 = setUpUser();
-//        usersDao.save(users1);
-//        Users users2 = setUpUser();
-//        usersDao.save(users2);
-//        assertEquals(3, usersDao.findAll().size());
-//        usersDao.clearAll();
-//        assertEquals(0, usersDao.findAll().size());
-//    }
-//    @Test
-//    public void getAllEmployeesInDepartment_returnsAllEmployeesInDepartment(){
-//        Departments departments = setUpDepartment();
-//        departmentsDao.save(departments);
-//        Departments departments1 = setUpDepartment();
-//        departmentsDao.save(departments1);
-//        Users users = setUpUserForDepartment(departments);
-//        usersDao.save(users);
-//        Users users1 = setUpUserForDepartment(departments);
-//        usersDao.save(users1);
-//        assertEquals(2, usersDao.allUsersInADepartment(departments.getId()).size());
-//    }
-//
-//    //helpers
-//    public Users setUpUser(){
-//        return new Users("Sonia","Tech", "Programmer",1);
-//    }
-//    public Departments setUpDepartment(){
-//        return new Departments("ICT","Programmer", 10);
-//    }
-//    public Users setUpUserForDepartment(Departments departments){
-//        return new Users("Sonia", "Tech","Programmer", departments.getId());
-//    }
-//}
-//
-//
-//
+package dao;
+
+import models.Departments;
+import models.Users;
+import org.junit.*;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+
+import static org.junit.Assert.*;
+
+public class Sql2oUsersDaoTest {
+    private static Sql2oUsersDao usersDao;
+    private static Sql2oDepartmentsDao departmentsDao;
+    private static Connection connection;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        String connectionString = "jdbc:postgresql://localhost:5432/organisational_api_test";
+        Sql2o sql2o = new Sql2o(connectionString, "makena", "barbie");
+        usersDao = new Sql2oUsersDao(sql2o);
+        departmentsDao = new Sql2oDepartmentsDao(sql2o);
+        connection = sql2o.open();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("CLEARING DATABASE!!!!!!");
+        usersDao.clearAll();
+        departmentsDao.clearAll();
+    }
+
+    @AfterClass
+    public static void shutDown(){
+        System.out.println("SHUTTING DOWN DATABASE!");
+        connection.close();
+    }
+
+    @Test
+    public void save_savesUserInformationInTheDatabase() throws Exception {
+        Users users = setUpUser();
+        usersDao.save(users);
+        assertEquals(1, usersDao.findAll().size());
+    }
+
+    @Test
+    public void findAll_ReturnsAllUsers() {
+        Users users = setUpUser();
+        assertEquals(0, usersDao.findAll().size());
+        usersDao.save(users);
+        assertEquals(1, usersDao.findAll().size());
+    }
+    @Test
+    public void findById_returnsExistingUsersBasedOnId() {
+        Users users = setUpUser();
+        usersDao.save(users);
+        Users users1 = usersDao.findById(users.getId());
+        assertEquals(users, users1);
+    }
+    @Test
+    public void delete_deletesUserById(){
+        Users users = setUpUser();
+        usersDao.save(users);
+        assertEquals(1, usersDao.findAll().size());
+        int idOfFoundUser = usersDao.findById(users.getId()).getId();
+        usersDao.delete(idOfFoundUser);
+        assertEquals(0, usersDao.findAll().size());
+    }
+    @Test
+    public void clearAll_DeletesEverything(){
+        Users users = setUpUser();
+        usersDao.save(users);
+        Users users1 = setUpUser();
+        usersDao.save(users1);
+        Users users2 = setUpUser();
+        usersDao.save(users2);
+        assertEquals(3, usersDao.findAll().size());
+        usersDao.clearAll();
+        assertEquals(0, usersDao.findAll().size());
+    }
+    @Test
+    public void getAllEmployeesInDepartment_returnsAllEmployeesInDepartment(){
+        Departments departments = setUpDepartment();
+        departmentsDao.save(departments);
+        Departments departments1 = setUpDepartment();
+        departmentsDao.save(departments1);
+        Users users = setUpUserForDepartment(departments);
+        usersDao.save(users);
+        Users users1 = setUpUserForDepartment(departments);
+        usersDao.save(users1);
+        assertEquals(2, usersDao.allUsersInADepartment(departments.getId()).size());
+    }
+
+    //helpers
+    public Users setUpUser(){
+        return new Users("Sonia","Tech", "Programmer",1);
+    }
+    public Departments setUpDepartment(){
+        return new Departments("ICT","Programmer", 10);
+    }
+    public Users setUpUserForDepartment(Departments departments){
+        return new Users("Sonia", "Tech","Programmer", departments.getId());
+    }
+}
+
+
+
--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,59 +1,59 @@
-//package models;
-//
-//import junit.framework.TestCase;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-//public class NewsTest  {
-//    @Test
-//    public void testNews_getTitle_String(){
-//        News news = new News("Group Work","group work", 7);
-//        assertEquals("Group Work",news.getTitle());
-//    }
-//
-//    @Test
-//    public void testNews_getContent_String(){
-//        News news = new News("Group Work","group work", 7);
-//        assertEquals("group work",news.getContent());
-//    }
-//
-//    @Test
-//    public void testNews_getDepartmentId_Int(){
-//        News news = new News("Group Work","group work", 7);
-//        assertEquals(7,news.getId());
-//    }
-//
-//    @Test
-//    public void setNewTitle_String(){
-//        News news = new News("Group Work","group work", 7);
-//        news.setTitle("Nihilism");
-//        assertEquals("Nihilism", news.getTitle());
-//    }
-//
-//    @Test
-//    public void setNewContent_String(){
-//        News news = new News("Group Work","group work", 7);
-//        news.setContent("Nihilism");
-//        assertEquals("philosophy", news.getContent());
-//    }
-//
-//    @Test
-//    public void setNewId_Int(){
-//        News news = new News("Group Work","group work", 7);
-//        news.setId(7);
-//        assertEquals(7, news.getId());
-//    }
-//
-//    @Test
-//    public void setNewDepartmentId_Int(){
-//        News news = new News("Group Work","group work", 7);
-//        news.setDepartmentId(7);
-//        assertEquals(7, news.getDepartmentId());
-//    }
-//
-//
-//
-//
-//
-//
-//    }
+package models;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NewsTest  {
+    @Test
+    public void testNews_getTitle_String(){
+        News news = new News("Group Work","group work", 7);
+        assertEquals("Group Work",news.getTitle());
+    }
+
+    @Test
+    public void testNews_getContent_String(){
+        News news = new News("Group Work","group work", 7);
+        assertEquals("group work",news.getContent());
+    }
+
+    @Test
+    public void testNews_getDepartmentId_Int(){
+        News news = new News("Group Work","group work", 7);
+        assertEquals(7,news.getId());
+    }
+
+    @Test
+    public void setNewTitle_String(){
+        News news = new News("Group Work","group work", 7);
+        news.setTitle("Nihilism");
+        assertEquals("Nihilism", news.getTitle());
+    }
+
+    @Test
+    public void setNewContent_String(){
+        News news = new News("Group Work","group work", 7);
+        news.setContent("Nihilism");
+        assertEquals("philosophy", news.getContent());
+    }
+
+    @Test
+    public void setNewId_Int(){
+        News news = new News("Group Work","group work", 7);
+        news.setId(7);
+        assertEquals(7, news.getId());
+    }
+
+    @Test
+    public void setNewDepartmentId_Int(){
+        News news = new News("Group Work","group work", 7);
+        news.setDepartmentId(7);
+        assertEquals(7, news.getDepartmentId());
+    }
+
+
+
+
+
+
+    }
